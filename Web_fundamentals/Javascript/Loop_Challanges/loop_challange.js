6812900a535080d8bb1729146bb3f2a32b8d2092
Bruno Marek <brunoamarek@hotmail.com>
2021-06-25 17:20:27 -0300

    loop_challange

---
+++ Web_fundamentals/Javascript/Loop_Challanges/loop_challange.js
@@ -0,0 +1,30 @@
+// Print odds 1-20
+for (var x = 0; x <= 20; x++) {
+    console.log (x);
+}
+
+// Decreasing Multiples of 3
+for (var i = 100; i >= 0; i--) {
+    if (i % 3 ===0) {
+        console.log (i);
+    }
+}
+
+// Print the sequence
+for (var z = 4; z > -3.6; z -=1.5) {
+    console.log(z)
+}
+
+// Sigma
+var sum = 0;
+for (var y = 0; y <= 100; y++){
+    sum += y;
+    console.log(sum)
+}
+
+// Factorial
+var product = 1;
+for (var m = 1; m <= 12; m++){
+    product *= m;
+    console.log(product)
+}
